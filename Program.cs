c9b272df9e82d38174efb4eb1859a6cf7725380b
Илья Алексеев <av98060@gmail.com>
2022-12-05 20:21:29 +0300

    Task 15

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿// //Task 10
-// Console.WriteLine("Task 10");
-// Console.WriteLine("Введите трехзначное число: ");
-// int a=int.Parse(Console.ReadLine());
-// int b=a/10;
-// b=b%10;
-// Console.WriteLine($"Вторая цифра числа {a} равна {b}");
+﻿//Task 10
+Console.WriteLine("Task 10");
+Console.WriteLine("Введите трехзначное число: ");
+int a=int.Parse(Console.ReadLine());
+int b=a/10;
+b=b%10;
+Console.WriteLine($"Вторая цифра числа {a} равна {b}");
 
 //Task 13
 Console.WriteLine("Task 13");
@@ -20,3 +20,27 @@ void number2 (int num)
 Console.WriteLine("Введите число: ");
 int c=int.Parse(Console.ReadLine());
 number2(c);
+
+//Task 15
+Console.WriteLine("Task 15");
+void day(int jk)
+{
+    if (jk<8 & jk>0) 
+    {
+        for (int i=1; i<8; i++)
+        {
+            if (jk==i & jk<6) 
+            {
+                Console.WriteLine("Это не выходной");
+            } else if (jk==i & jk>5)
+            {
+                Console.WriteLine("Сегодня выходной");
+            }
+        }
+    } else
+    {
+        Console.WriteLine("Такого дня недели не существует");
+    }
+}
+Console.WriteLine("Введите число дня недели: ");
+day(int.Parse(Console.ReadLine()));
